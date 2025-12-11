53fc43f6a818db247f2b8f6fe5fda668075293d1
mo7ammedash <mohammedashour664@gmil.com>
2025-12-11 15:05:11 +0200

    Initial commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Laravel Hello</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <h2>Hello, {{ $name }}</h2>
+</body>
+</html>
