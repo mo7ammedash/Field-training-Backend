4d406c73581a0b9c80d6f2df9aa6b26aee67363a
mo7ammedash <mohammedashour664@gmil.com>
2025-12-14 15:26:52 +0200

    Add w02 Task

---
+++ w02/storage/framework/views/b3e60a167009987cc44b3332b35e285d.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1><?php echo e($title); ?></h1>
+</body>
+</html>
+<?php /**PATH D:\Course Laravel\w02\resources\views/home.blade.php ENDPATH**/ ?>
