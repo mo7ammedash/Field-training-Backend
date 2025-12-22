648366e4b3d389d646b4935ecc35e0bc7e6dd799
mo7ammed N Ashour <mohammedashour664@gmail.com>
2025-12-22 18:59:38 +0200

    Delete w04 directory

--- w04/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-
-Route::get('/', [ProductController::class, 'index'])->name('products.index');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
